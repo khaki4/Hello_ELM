aa2817192387259c7f72cdfa9851a8247c17098a
정지원 <4dakto@gmail.com>
2017-02-11 21:14:27 +0900

    Jiwon ::  Write functions and Type Signatures in Elm

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ELM tutorial
 
-## Inital settings
+## Initial settings
  - elm install: https://guide.elm-lang.org/install.html
+ - elm npm i -g elm
  - elm reactor
  - elm package install elm-lang/html
--- hello.elm
+++ hello.elm
@@ -1,5 +1,20 @@
 module Main exposing (..)
 
 import Html exposing(text)
+
+politely : String -> String
+politely phrase =
+    "Excuse me, " ++ phrase
+
+ask : String -> String -> String
+ask thing place =
+    "is there a "
+    ++ thing
+    ++ " in the "
+    ++ place
+    ++ "?"
+
+askPolitelyAboutFish : String -> String
+askPolitelyAboutFish = politely << (ask "fish")
 main =
-    text "Hello, elm"
+    text <| askPolitelyAboutFish "hat"
